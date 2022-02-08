0921aa35da0421fc343ce3ce78a0f56095f75f25
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2022-02-08 17:53:37 +0000

    Implemented walk and attack SFX for the Ghoul.

--- Assets/Scripts/Morgue/GhoulChasePlayer.cs
+++ Assets/Scripts/Morgue/GhoulChasePlayer.cs
@@ -5,6 +5,11 @@ using UnityEngine.AI;
 public class GhoulChasePlayer : MonoBehaviour
 {
     private bool canChasePlayer = false;
+
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip walkSFX;
+    [SerializeField] private AudioClip attackSFX;
+    [SerializeField] private AudioClip deathSFX;
     [SerializeField] private Animation ghoulAnimation;
     [SerializeField] private NavMeshAgent ghoulAgent;
     [SerializeField] private Transform player;
@@ -14,29 +19,55 @@ public class GhoulChasePlayer : MonoBehaviour
     {
         ghoulAgent.speed = 0;
         ghoulAnimation.Play("Idle");
+
     }
 
 
     private void Update()
     {
+        // If the chase event has been called which sets the canChasePlayer bool to true
         if (canChasePlayer)
         {
+            // Move the Ghoul towards the Player at the speed of the Nav Agent
             ghoulAgent.SetDestination(player.position);
 
+            // If the Ghoul is not moving so has stopped at the Player
             if (ghoulAgent.velocity.x == 0 && ghoulAgent.velocity.z == 0)
             {
-                ghoulAnimation.Stop("Walk");
+                // If the walking SFX clip is playing then stop it so the attack clip can play as soon as the Ghoul attacks
+                if (audioSource.clip == walkSFX)
+                    audioSource.Stop();
+
+                // Wait till a clip has stopped playing untill playing it again
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.clip = attackSFX;
+                    audioSource.Play();
+                }
+
+                // Play Attack animation
                 ghoulAnimation.Play("Attack1");
             }
-            else
+            else // If the Ghoul is chasing the Player
             {
-                ghoulAnimation.Stop("Attack1");
+                // Wait till a clip has stopped playing untill playing it again
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.clip = walkSFX;
+                    audioSource.Play();
+                }
+
+                // Play Attack animation
                 ghoulAnimation.Play("Walk");
             }
         }
     }
 
 
+    /// <summary>
+    /// Called by the Game Event Listener Component attached to this game object
+    /// Sets the chase bool to true and sets the movement speed
+    /// </summary>
     public void ChasePlayer()
     {
         canChasePlayer = true;
